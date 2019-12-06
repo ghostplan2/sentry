2cf9f37713e85ab21b0109e72626fe81b8532122
ghostplan2 <ghost_plan_2@mail.com>
2019-12-06 19:21:32 +0400

    change button text

--- react-app/src/Hello/index.js
+++ react-app/src/Hello/index.js
@@ -16,7 +16,7 @@ class Hello extends React.Component {
     render() {
         return (
             <div>
-                <button onClick={this.handleClick}>21121</button>
+                <button onClick={this.handleClick}>Hi Click Here</button>
             </div>
         );
     }
