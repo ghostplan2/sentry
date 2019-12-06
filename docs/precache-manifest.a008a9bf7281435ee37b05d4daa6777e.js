3eb9f5308d03b1ab7b16b0a4bbef238af7592efc
ghostplan2 <ghost_plan_2@mail.com>
2019-12-06 18:37:30 +0400

    update packagejson

---
+++ docs/precache-manifest.a008a9bf7281435ee37b05d4daa6777e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f9e611e111a4fad49018cd24fd7de8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc3b0a2c1844f45ae792",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d714f222de0ba27b4dbf",
+    "url": "/static/js/2.f32e8eef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f32e8eef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc3b0a2c1844f45ae792",
+    "url": "/static/js/main.1c3d83b9.chunk.js"
+  },
+  {
+    "revision": "a2076e2b6ff597e10196",
+    "url": "/static/js/runtime-main.30cba478.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
